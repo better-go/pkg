263e9e7dbb8b901ba609528743c8a5bfd7ce9960
hhstore <hhglory@outlook.com>
2020-06-09 17:23:53 +0800

    update

--- time/time.go
+++ time/time.go
@@ -7,53 +7,58 @@ import (
 	stdTime "time"
 )
 
+////////////////////////////////////////////////////////////////////////////////////////
+
 // Time be used to MySql timestamp converting.
 type Time int64
 
 // Scan scan time.
-func (jt *Time) Scan(src interface{}) (err error) {
+func (m *Time) Scan(src interface{}) (err error) {
 	switch sc := src.(type) {
 	case stdTime.Time:
-		*jt = Time(sc.Unix())
+		*m = Time(sc.Unix())
 	case string:
 		var i int64
 		i, err = strconv.ParseInt(sc, 10, 64)
-		*jt = Time(i)
+		*m = Time(i)
 	}
 	return
 }
 
 // Value get time value.
-func (jt Time) Value() (driver.Value, error) {
-	return stdTime.Unix(int64(jt), 0), nil
+func (m Time) Value() (driver.Value, error) {
+	return stdTime.Unix(int64(m), 0), nil
 }
 
 // Time get time.
-func (jt Time) Time() stdTime.Time {
-	return stdTime.Unix(int64(jt), 0)
+func (m Time) Time() stdTime.Time {
+	return stdTime.Unix(int64(m), 0)
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+
 // Duration be used toml unmarshal string time, like 1s, 500ms.
+// extend for toml parse.
 type Duration stdTime.Duration
 
 // UnmarshalText unmarshal text to duration.
-func (d *Duration) UnmarshalText(text []byte) error {
+func (m *Duration) UnmarshalText(text []byte) error {
 	tmp, err := stdTime.ParseDuration(string(text))
 	if err == nil {
-		*d = Duration(tmp)
+		*m = Duration(tmp)
 	}
 	return err
 }
 
 // Shrink will decrease the duration by comparing with context's timeout duration
 // and return new timeout\context\CancelFunc.
-func (d Duration) Shrink(c context.Context) (Duration, context.Context, context.CancelFunc) {
+func (m Duration) Shrink(c context.Context) (Duration, context.Context, context.CancelFunc) {
 	if deadline, ok := c.Deadline(); ok {
-		if ctimeout := stdTime.Until(deadline); ctimeout < stdTime.Duration(d) {
+		if expire := stdTime.Until(deadline); expire < stdTime.Duration(m) {
 			// deliver small timeout
-			return Duration(ctimeout), c, func() {}
+			return Duration(expire), c, func() {}
 		}
 	}
-	ctx, cancel := context.WithTimeout(c, stdTime.Duration(d))
-	return d, ctx, cancel
+	ctx, cancel := context.WithTimeout(c, stdTime.Duration(m))
+	return m, ctx, cancel
 }
