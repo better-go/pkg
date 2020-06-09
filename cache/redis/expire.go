b0035d2edf73f03e1229bc7c3db71e81fa32c0d1
hhstore <hhglory@outlook.com>
2020-06-09 18:24:18 +0800

    update

--- cache/redis/expire.go
+++ cache/redis/expire.go
@@ -3,53 +3,53 @@ package redis
 import "time"
 
 type Expiration struct {
-	Second uint32
-	Minute uint32
-	Hour   uint32
-	Day    uint32
-	Week   uint32
-	Month  uint32
+	second uint32
+	minute uint32
+	hour   uint32
+	day    uint32
+	week   uint32
+	month  uint32
 }
 
 func NewExpiration() *Expiration {
 	return &Expiration{
-		Second: 1,
-		Minute: 1 * 60,
-		Hour:   1 * 60 * 60,
-		Day:    1 * 60 * 60 * 24,
-		Week:   1 * 60 * 60 * 24 * 7,
-		Month:  1 * 60 * 60 * 24 * 31,
+		second: 1,
+		minute: 1 * 60,
+		hour:   1 * 60 * 60,
+		day:    1 * 60 * 60 * 24,
+		week:   1 * 60 * 60 * 24 * 7,
+		month:  1 * 60 * 60 * 24 * 31,
 	}
 }
 
 // 以秒为单位:
 func (m *Expiration) Seconds(num uint32) time.Duration {
-	return calcSeconds(m.Second, num)
+	return calcSeconds(m.second, num)
 }
 
 // 以分钟为单位:
 func (m *Expiration) Minutes(num uint32) time.Duration {
-	return calcSeconds(m.Minute, num)
+	return calcSeconds(m.minute, num)
 }
 
 // 以小时为单位:
 func (m *Expiration) Hours(num uint32) time.Duration {
-	return calcSeconds(m.Hour, num)
+	return calcSeconds(m.hour, num)
 }
 
 // 以日为单位:
 func (m *Expiration) Days(num uint32) time.Duration {
-	return calcSeconds(m.Day, num)
+	return calcSeconds(m.day, num)
 }
 
 // 以周为单位:
 func (m *Expiration) Weeks(num uint32) time.Duration {
-	return calcSeconds(m.Week, num)
+	return calcSeconds(m.week, num)
 }
 
 // 以月为单位:
 func (m *Expiration) Months(num uint32) time.Duration {
-	return calcSeconds(m.Month, num)
+	return calcSeconds(m.month, num)
 }
 
 //////////////////////////////////////////////////////////////////////////
