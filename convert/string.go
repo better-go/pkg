eb13081df02f07d5b18766f8b3e77df70defad07
hhstore <hhglory@outlook.com>
2020-06-29 06:05:27 +0800

    update

--- convert/string.go
+++ convert/string.go
@@ -2,8 +2,18 @@ package convert
 
 import "strconv"
 
+func IntToString(i int) string {
+	return strconv.Itoa(i)
+}
+
 func Int64ToString(i int64) string {
 	return strconv.FormatInt(i, 10)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+func StringToInt(s string) (int, error) {
+	return strconv.Atoi(s)
 }
 
 func StringToInt64(s string) (int64, error) {
--- convert/string_test.go
+++ convert/string_test.go
@@ -3,19 +3,31 @@ package convert
 import "testing"
 
 func TestInt64ToString(t *testing.T) {
-	var i int64
-	var s string
-	i = 60
+	var i1 int64
+	var i2 int
+	var s1, s2 string
 
-	s = Int64ToString(i)
-	t.Log("s=", s)
+	i1 = 60
+	i2 = 30
+
+	s1 = Int64ToString(i1)
+	s2 = IntToString(i2)
+
+	t.Log("s1=", s1)
+	t.Log("s2=", s2)
 
 }
 
 func TestStringToInt64(t *testing.T) {
-	var i int64
-	s := "2233"
+	var i1 int64
+	var i2 int
 
-	i, _ = StringToInt64(s)
-	t.Log("i=", i)
+	s1 := "2233"
+	s2 := "3456"
+
+	i1, _ = StringToInt64(s1)
+	i2, _ = StringToInt(s2)
+
+	t.Log("i1=", i1)
+	t.Log("i2=", i2)
 }
