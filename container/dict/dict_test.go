367501e9898abf3053edf52ae8aceb6dc2d96dc4
hhstore <hhglory@outlook.com>
2021-03-23 13:00:13 +0800

    update

--- container/dict/dict_test.go
+++ container/dict/dict_test.go
@@ -5,17 +5,34 @@ import (
 )
 
 func TestDict_Encode(t *testing.T) {
-	// 字段必须是大写:
-	in := Dict{
+	in := []Dict{
+		{
+			"name":     "jim",
+			"age":      "20",
+			"address":  "shanghai",
+			"children": []string{"bob", "jackie"}, // TODO: 无法正常处理, 注意
+			"dog":      "frank",
+		},
+		{
+			"name":     "jim",
+			"age":      "20",
+			"address":  "shanghai-pudong",
+			"children": "bob",
+			"dog":      "frank",
+		},
 
-		"name":     "jim",
-		"age":      20,
-		"address":  "shanghai",
-		"children": []string{"bob", "jackie"},
-		"dog":      "frank",
+		{
+			"name":     "jim",
+			"age":      "20",
+			"address":  "shanghai-pudong",
+			"children": []byte("bob"), // TODO: 无法正常处理, 注意
+			"dog":      "frank",
+		},
 	}
 
-	resp := in.Encode()
-	t.Logf("in: %+v", in)
-	t.Logf("convert to url string: %+v", resp)
+	for _, item := range in {
+		resp := item.Encode()
+		t.Logf("in: %+v", item)
+		t.Logf("convert to url string: %+v", resp)
+	}
 }
--- container/dict/string_test.go
+++ container/dict/string_test.go
@@ -1,1 +1,28 @@
 package dict
+
+import "testing"
+
+func TestStringDict_Encode(t *testing.T) {
+	in := []StringDict{
+		{
+			"name":     "jim",
+			"age":      "20",
+			"address":  "shanghai",
+			"children": "bob, kate, tom",
+			"dog":      "frank",
+		},
+		{
+			"name":     "jim",
+			"age":      "20",
+			"address":  "shanghai-pudong",
+			"children": "bob",
+			"dog":      "frank",
+		},
+	}
+
+	for _, item := range in {
+		resp := item.Encode()
+		t.Logf("in: %+v", item)
+		t.Logf("convert to url string: %+v", resp)
+	}
+}
